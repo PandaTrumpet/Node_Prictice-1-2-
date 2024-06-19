700b905b3750d3bd4a89d756a731fb5b4d1c52c7
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-19 13:24:43 +0300

    Add init

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import { env } from './utils/env.js';
 export const setupServer = () => {
-
-    const
+  const app = express();
+  const PORT = Number(env('PORT', '3000'));
+  app.listen(PORT, () => {
+    console.log('Success port!');
+  });
 };
+setupServer();
